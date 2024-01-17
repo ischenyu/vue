80dc57612204710d30b8bf578e57c8a7839669a2
root <3469134108@qq.com>
2024-01-17 14:00:11 +0800

    1.17

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <h1>一、模板语法</h1>
+  <p class='p'>{{ msg }}</p> <!--模板语法，标签中的msg显示了return中的内容-->
+  <p>{{ number +1 }}</p> <!--可以被求值的JavaScript代码也是可以放的，建议在JavaScript中直接返回-->
+  <p>{{ ok ? "“ok”的值为真" : "“ok”的值为假" }}</p> <!--JavaScript表达式也可以，建议在JavaScript中直接返回-->
+  <p>{{ message.split('').reverse().join('') }}</p> <!--也可以是复杂的表达式，建议在JavaScript中直接返回-->
+  <p>{{ rawHtml }}</p> <!--v-pre指令，跳过这个元素和它的子元素的编译过程，直接显示-->
+  <p v-html="rawHtml"></p> <!--v-html指令，内容按HTML格式输出-->
+  <p>二、属性绑定</p>
+  <div v-bind:id="appID" v-bind:class="dynamicClass">AppID</div> <!--绑定属性用“v-bind:”-->
+</template>
+
+<style>
+.active{
+  color:blue;
+  font-size:20px;
+  background-color:yellow;
+  width:100px;
+  height:100px;
+  border:1px solid black;
+  margin:10px;
+  padding:10px;
+  display:inline-block;
+  text-align:center;
+  line-height:100px;
+  border-radius:10px;
+  box-shadow: 0 0 10px #888;
+  cursor:pointer;
+  user-select:none;
+  position:relative;
+  top:0;
+  left:0;
+  transition:all 0.5s ease;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello, Vue!',
+      number: 10,
+      ok: true,
+      message: '大家好啊',
+      rawHtml: '<span style="color:red">这里会显示红色</span>',
+      dynamicClass: 'active'
+    }
+  }
+}
+</script>
