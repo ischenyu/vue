d08aee1d25f12212ad6bbe4f7874c8fd25bc3f57
ischenyu <3469134108@qq.com>
2024-01-24 20:12:30 +0800

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <layui-icon type="layui-icon-github" color="#009688"></layui-icon>
   <h1>一、模板语法</h1>
   <p class='p'>{{ msg }}</p> <!--模板语法，标签中的msg显示了return中的内容-->
   <p>{{ number +1 }}</p> <!--可以被求值的JavaScript代码也是可以放的，建议在JavaScript中直接返回-->
@@ -10,18 +9,12 @@
   <h1>二、属性绑定</h1>
   <p>下面是动态绑定的例子，这个`div`组件展示了如何进行元素的属性绑定，具体查看源代码</p>
   <div v-bind:id="appID" v-bind:class="dynamicClass">AppID</div> <!--绑定属性用“v-bind:”-->
-  <h1>事件绑定</h1>
-  <lay-button-container>
-      <lay-button type="primary" :loading="loadState" @click="loadStateClick">加载</lay-button>
-      <lay-button type="default" :loading="loadState" loadingIcon="layui-icon-loading">加载</lay-button>
-      <lay-switch v-model="loadState"></lay-switch>
-  </lay-button-container>
-  <lay-button type="default" @click="clickHandle">单击事件</lay-button>
-  <layui-icon class="upload-icon" type="layui-icon-upload" ></layui-icon>
-  <lay-button type="primary" @click="openNotifiy">通知</lay-button>
+  <lay-button type="normal" :disabled="isbuttondisabled">Button</lay-button><!--布尔值Attribute-->
+  <div v-bind="objectOfAttrs">div</div>
 </template>
 
 <script>
+import { ref } from 'vue';
 export default {
   data() {
     return {
@@ -30,30 +23,14 @@ export default {
       ok: true,
       message: '大家好啊',
       rawHtml: '<span style="color:red">这里会显示红色</span>',
-      dynamicClass: 'active'
+      dynamicClass: 'active',
+      isbuttondisabled: false,
+      objectOfAttrs: {
+        id: 'appclass',
+	class: 'adiv'
+      }
     }
   }
-}
-</script>
-
-<script setup>
-import { ref } from 'vue';
-
-const loadState = ref(true);
-
-const loadStateClick = () => {
-  alert("触发")
-}
-const clickHandle = () => {
-  console.log('点击事件');
-  alert("pressed");
-}
-const openNotifiy = function() {
-    layer.notifiy({
-      title:"标题",
-      content:"默认就是右上，也是用得最多的"
-    });
-    alert("message");
 }
 </script>
 
@@ -78,6 +55,12 @@ const openNotifiy = function() {
   top:0;
   left:0;
   transition:all 0.5s ease;
+}
+
+.adiv{
+  font-size:22px;
+  color:green;
+
 }
 
 </style>
